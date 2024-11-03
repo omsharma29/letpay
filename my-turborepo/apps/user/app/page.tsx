22d00cf37f1d0d8b82d5ff1353fb4de67a85b81c
omsharma29 <151984518+omsharma29@users.noreply.github.com>
2024-11-03 13:04:04 +0530

    tailwind added

---
+++ my-turborepo/apps/user/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@repo/ui/button'
+
+function Home() {
+  return (
+    <>
+    <div>HomePage</div>
+    <Button href='/dashboard' >DashBoard</Button>
+    </>
+  )
+}
+
+export default Home
--- my-turborepo/packages/ui/src/button.tsx
+++ my-turborepo/packages/ui/src/button.tsx
@@ -1,20 +1,24 @@
 "use client";
 
 import { ReactNode } from "react";
+import Link from 'next/link'
+
 
 interface ButtonProps {
   children: ReactNode;
   className?: string;
-  appName: string;
+  href?: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
+export const Button = ({ children, className, href }: ButtonProps) => {
+  return href? (
+    <Link href={href}>
+      <button>{children}</button>
+    </Link>
+  ):
+  
+    <button className={className}>
       {children}
     </button>
-  );
+  
 };
